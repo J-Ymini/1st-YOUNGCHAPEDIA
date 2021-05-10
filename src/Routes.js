52c184c41f9ede9b2af139d7f5a68726fe65c050
yongminJeon <dydalsdl1414@gmail.com>
2021-05-10 19:41:00 +0900

    Add: 초기세팅완료

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main/Main';
+// import login
+// import mypage
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main}></Route>
+          {/* <Route exact path="/contents" component={}></Route> */}
+          {/* <Route exact path="/myPage" component={}></Route> */}
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return <div>"초기세팅입니다."</div>;
+  }
+}
+
+export default Main;
