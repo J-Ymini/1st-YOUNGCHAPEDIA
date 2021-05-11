c7140de9b7b765d11aa5e8436816754536810a28
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-12 00:37:25 +0900

    Add : 공통 컴포넌트 Navbar 레이아웃 1차 완성

--- src/pages/CommonComponents/Navbar.js
+++ src/pages/CommonComponents/Navbar.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.scss';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUserLogin: false,
+    };
+  }
   render() {
+    const { isUserLogin } = this.state;
     return (
-      <nav>
-        {/* 이거 로고 컴포넌트로 해야하는지? */}
-        <span>
+      <nav className="topNav">
+        <span className="logoMenu">
           <header>
-            <h1>
-              <span>YOUNGCHA</span>
-              <span>PEDIA</span>
-            </h1>
+            <Link to="/">
+              <h1>
+                <span>YOUNGCHA</span>
+                <span>PEDIA</span>
+              </h1>
+            </Link>
           </header>
           <ul className="navMenu">
             <li>영화</li>
           </ul>
         </span>
-        <span>
-          <input placeholder="작품 제목, 배우, 감독을 검색해보세요"></input>
+        <span className="searchUser">
+          <label className="navSearch">
+            <FontAwesomeIcon icon={faSearch} className="topNavIcon" />
+            <input placeholder="작품 제목, 배우, 감독을 검색해보세요" />
+          </label>
+          <button className={`navLoginBtn ${isUserLogin && 'topNavHidden'}`}>
+            로그인
+          </button>
+          <button className={`navLogoutBtn ${isUserLogin || 'topNavHidden'}`}>
+            로그아웃
+          </button>
+          <button className={`navSignInBtn ${isUserLogin && 'topNavHidden'}`}>
+            회원가입
+          </button>
+          <button className={isUserLogin || 'topNavHidden'}>
+            <div className="navUserProfile"></div>
+          </button>
         </span>
       </nav>
     );
