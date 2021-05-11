dfcae88da770143d0e9a3960e2544a546fc0fad2
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-11 15:05:40 +0900

    Modify : 컨플릭트 방지 - Routes 초기와 똑같이 수정

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
-import Footer from './pages/CommonComponents/Footer';
 // import login
 // import mypage
 
@@ -11,7 +10,7 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Footer}></Route>
+          {/* <Route exact path="/" component={}></Route> */}
           {/* <Route exact path="/contents" component={}></Route> */}
           {/* <Route exact path="/myPage" component={}></Route> */}
         </Switch>
