e3cf68cf8b4ac3da92a82cc15e099ec82d6d0cd7
yongminJeon <dydalsdl1414@gmail.com>
2021-05-11 16:14:13 +0900

    Modifying: 영화 상세 페이지 레이아웃 구현중 백업

--- src/pages/MovieDetail/MovieDetail.js
+++ src/pages/MovieDetail/MovieDetail.js
@@ -6,21 +6,96 @@ export default class MovieDetail extends React.Component {
     return (
       <main className="MovieDetail">
         <section className="movieBannerSection">
-          <div className="movieBannerPoster"></div>
-          <div className="movieSimpleInformation">글자입니다.</div>
-        </section>
-        <div className="movieDetailContents">
-          <section className="leaveCommentSection">
-            <header>
-              <h2>기본정보</h2>
-              <div>
-                <a></a>
+          <div className="movieBannerPoster">
+            <div className="movieBannerAndBookRating"></div>
+          </div>
+          <div className="movieSimpleInformation">
+            <div className="movieSimpleInformationContents">
+              <img
+                alt="poster-img"
+                src="https://img.hankyung.com/photo/202104/BF.26103128.1.jpg"
+              />
+              <div className="movieBookRating">예매 순위: 2위</div>
+              <div className="movieSubject">
+                <h1>분노의 질주: 더 얼티메이트</h1>
+                <div className="movieGenre">
+                  2020 &middot; 스릴러 &middot; 미국
+                </div>
+                <div className="averageOfStarPoint">
+                  평균 <i class="fas fa-star"></i> 3.8(192명)
+                </div>
+                <div className="wishAndStarPoint">
+                  <button className="wishButton">
+                    <i class="fas fa-plus"></i> 보고싶어요
+                  </button>
+                  <div>
+                    평가하기
+                    <div>
+                      <i class="far fa-star" />
+                      <i class="far fa-star" />
+                      <i class="far fa-star" />
+                      <i class="far fa-star" />
+                      <i class="far fa-star" />
+                    </div>
+                  </div>
+                </div>
               </div>
-            </header>
-          </section>
-          <section className="movieInformationSection"></section>
-          <section className="commentSection"></section>
-        </div>
+            </div>
+          </div>
+        </section>
+        <section className="movieDetailContentsSection">
+          <div className="movieDetailContents">
+            <article className="leaveComment"></article>
+            <article className="movieInformation">
+              <div className="movieInformationContents">
+                <header>
+                  <h2>기본정보</h2>
+                  <div>
+                    <a>더보기</a>
+                  </div>
+                </header>
+                <article>
+                  <div>
+                    F9
+                    <br />
+                    <span>2020 &middot; 미국 &middot; 스릴러</span>
+                    <br />
+                    2시간 15분
+                  </div>
+                  <p>
+                    기다림은 끝났다! 전 세계가 기다려온 단 하나의
+                    액션블록버스터! 도미닉(빈 디젤)은 자신과 가장 가까웠던 형제
+                    제이콥(존 시나)이 사이퍼(샤를리즈 테론)와 연합해 전 세계를
+                    위기로 빠트릴 위험천만한 계획을 세운다는 사실을 알게
+                    되고,...
+                  </p>
+                </article>
+              </div>
+            </article>
+            <article className="comments">
+              <div className="commentContents">
+                <header>
+                  <div>
+                    <h2>코멘트</h2>
+                    <span>20+</span>
+                  </div>
+                  <button>
+                    <a>더보기</a>
+                  </button>
+                </header>
+                <article>코멘트 컴포넌트</article>
+              </div>
+            </article>
+            <article className="similarMovie">
+              <div className="similarMovieContents">
+                <header>
+                  <h2>비슷한 작품</h2>
+                </header>
+                <article></article>
+              </div>
+            </article>
+          </div>
+        </section>
       </main>
     );
   }
