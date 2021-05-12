76d5451af22abda6e2203f7c63d590d972806d84
yongminJeon <dydalsdl1414@gmail.com>
2021-05-12 14:31:04 +0900

    <20기 전용민> Modifying: 영화 상세페이지 레이아웃 1차 리뷰 반영 conflict 해결

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Main from './pages/Main/Main';
 import Footer from './pages/CommonComponents/Footer';
 // import login
 // import mypage
-import Navbar from './pages/CommonComponents/Navbar';
 import MovieDetail from './pages/MovieDetail/MovieDetail';
 class Routes extends React.Component {
   render() {
@@ -12,10 +12,11 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main}></Route>
+          {/* <Route exact path="/movieDetail" component={MovieDetail}></Route> */}
           {/* <Route exact path="/contents" component={}></Route> */}
           {/* <Route exact path="/myPage" component={}></Route> */}
-          {/* <Route exact path="/movieDetail" component={MovieDetail}></Route> */}
         </Switch>
+        <Footer />
       </Router>
     );
   }
