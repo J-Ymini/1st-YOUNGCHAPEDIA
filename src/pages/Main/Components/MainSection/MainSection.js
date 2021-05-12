0f64880c8af9e7b1b7c49c81b996aa7bf2c2df6c
Daseul <dls2tmfs2@gmail.com>
2021-05-12 18:36:08 +0900

    Modify : mockdata fetch

--- src/pages/Main/Components/MainSection/MainSection.js
+++ src/pages/Main/Components/MainSection/MainSection.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import MainSectionMovieList from '../MainSectionMovieList/MainSectionMovieList';
 import './MainSection.scss';
-
 export default class MainSection extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movieInfomationList: [],
+    };
+  }
+  componentDidMount() {
+    fetch('http://localhost:3000/data/MainMockData.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(movieInfomation => {
+        this.setState({
+          movieInfomationList: movieInfomation,
+        });
+      });
+  }
   render() {
+    const { movieInfomationList } = this.state;
     return (
       <section className="mainSection">
         <div className="mainTitle">
@@ -11,7 +28,17 @@ export default class MainSection extends React.Component {
         </div>
         <section className="mainMovie">
           <ul>
-            <MainSectionMovieList />
+            {movieInfomationList.map(movie => {
+              return (
+                <MainSectionMovieList
+                  key={movie.id}
+                  title={movie.korean_title}
+                  country={movie.country}
+                  releaseDate={movie.release_date}
+                  thumbnailImgUrl={movie.thumbnail_img}
+                />
+              );
+            })}
           </ul>
         </section>
       </section>
--- src/pages/Main/Components/MainSectionMovieList/MainSectionMovieList.js
+++ src/pages/Main/Components/MainSectionMovieList/MainSectionMovieList.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './MainSectionMovieList.scss';
-
 export default class MainSectionMovieList extends React.Component {
   render() {
+    const { title, country, releaseDate, thumbnailImgUrl } = this.props;
     return (
       <li className="mainSectionMovieList">
-        <ol>
-          <li className="listRanking">1</li>
-        </ol>
-        <img
-          alt="test"
-          src="https://movie-phinf.pstatic.net/20210421_37/1618971733493B4ykS_JPEG/movie_image.jpg"
-        />
+        <div className="listRanking">1</div>
+        <img alt="test" src={thumbnailImgUrl} />
         <div className="listDescription">
-          <p className="listDescriptionTitle">분노의 질주: 더 얼티메이트</p>
-          <p className="listDescriptionYear">2020 ・ 미국</p>
+          <p className="listDescriptionTitle">{title}</p>
+          <p className="listDescriptionYear">
+            {country}・{releaseDate}
+          </p>
           <p className="listDescriptionGrade">평균 3.8</p>
         </div>
       </li>
