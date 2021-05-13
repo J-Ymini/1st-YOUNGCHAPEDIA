e687957cb9f9e7499e0371677b435e5cea9af32a
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-14 00:34:09 +0900

    Modify : nav와 맞추기 위해 z-index수정, 주석 설명 추가

--- src/pages/CommonComponents/Modal.js
+++ src/pages/CommonComponents/Modal.js
@@ -25,6 +25,7 @@ export default class Modal extends Component {
       <>
         <div className="modalContainer">
           <div ref={this.modalRef} className="modal">
+            {/* 여기에 원하는 내용 넣어 사용 - 콘텐츠에 margin/padding등을 넣어 modal 크기 조정하기, 아래는 예시*/}
             <p>안녕하세요</p>
           </div>
           <div className="modalBackground" onClick={this.closeModal} />
