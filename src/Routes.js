20bcb71b4414367a0a8b4d954f8f7459be1d7eec
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-14 11:15:24 +0900

    Add : LoginSignIn Form 레이아웃, 유효성검사

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
-import Footer from './pages/CommonComponents/Footer';
+import LoginSignInForm from './pages/CommonComponents/LoginSignInForm';
 // import login
 // import mypage
 import Navbar from './pages/CommonComponents/Navbar';
@@ -11,13 +11,13 @@ class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
         <Switch>
-          <Route exact path="/" component={Main}></Route>
+          <Route exact path="/" component={LoginSignInForm}></Route>
           {/* <Route exact path="/contents" component={}></Route> */}
           {/* <Route exact path="/myPage" component={}></Route> */}
         </Switch>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
     );
   }
