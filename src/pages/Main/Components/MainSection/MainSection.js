2cc63405755b3813f225bd692dcd435bc61bc91c
Daseul <dls2tmfs2@gmail.com>
2021-05-15 17:41:29 +0900

    Add : 백엔드 데이터 GET Setting

--- src/pages/Main/Components/MainSection/MainSection.js
+++ src/pages/Main/Components/MainSection/MainSection.js
@@ -5,98 +5,79 @@ export default class MainSection extends React.Component {
   constructor() {
     super();
     this.state = {
-      movieInformationList: [],
+      // movieInformationList: [],
       width: '',
-      index: 1,
+      index: 0,
     };
     this.containerWidth = React.createRef();
   }
 
-  componentDidMount() {
-    fetch('http://localhost:3000/data/MainMockData.json', {
-      method: 'GET',
-    })
-      .then(res => res.json())
-      .then(movieInfomationList => {
-        this.setState({
-          movieInformationList: movieInfomationList,
-        });
-      });
-  }
-
   //다음 슬라이드 이동 함수
   handleNextSliding = () => {
-    const { movieInformationList, index } = this.state;
+    const { index } = this.state;
+    const { movieInformationList } = this.props;
 
     if (index === Math.floor(movieInformationList.length / 5)) {
       return;
     }
     this.setState({
       index: index + 1,
-      width: this.containerWidth.current.scrollWidth * index,
+      width: this.containerWidth.current.scrollWidth * (index + 1),
     });
   };
 
   //이전 슬라이드 이동 함수
   handlePreSliding = () => {
     const { index } = this.state;
-    if (index === 1) {
+    if (index === 0) {
       return;
     }
     this.setState({
       index: index - 1,
-      width: this.containerWidth.current.scrollWidth * (index - 2),
+      width: this.containerWidth.current.scrollWidth * (index - 1),
     });
   };
 
   render() {
-    const { movieInformationList, width, index } = this.state;
-    const keys = Object.keys(movieInformationList)[0];
-    // 조건부 렌더링
-    if (movieInformationList.length === 0) {
-      return null;
-    } else {
-      return (
-        <section className="mainSection">
-          <div className="mainTitle">
-            <p>{keys}</p>
-          </div>
-          <button
-            disabled={index === 1 && 'disabled'} //초기값일때 이전버튼 비활성화
-            onClick={this.handlePreSliding}
-            className="mainMoviePreBtn"
-          >
-            <i class="fas fa-chevron-left"></i>
-          </button>
-          <section className="mainMovie" ref={this.containerWidth}>
-            <ul>
-              {movieInformationList[keys].map(movie => {
-                const {
-                  id,
-                  korean_title,
-                  country,
-                  release_date,
-                  thumbnail_img,
-                } = movie;
-                return (
-                  <MainSectionMovieList
-                    width={width}
-                    key={id}
-                    title={korean_title}
-                    country={country}
-                    releaseDate={release_date}
-                    thumbnailImgUrl={thumbnail_img}
-                  />
-                );
-              })}
-            </ul>
-            <div></div>
-          </section>
-          <button onClick={this.handleNextSliding} className="mainMovieNextBtn">
-            <i class="fas fa-chevron-right"></i>
-          </button>
+    const { width, index } = this.state;
+    const { listTitle, movieInformationList } = this.props;
+    return (
+      <section className="mainSection">
+        <div className="mainTitle">
+          <p>{listTitle}</p>
+        </div>
+        <button
+          disabled={index === 0 && 'disabled'} //초기값일때 이전버튼 비활성화
+          onClick={this.handlePreSliding}
+          className="mainMoviePreBtn"
+        >
+          <i class="fas fa-chevron-left"></i>
+        </button>
+        <section className="mainMovie" ref={this.containerWidth}>
+          <ul>
+            {movieInformationList.map(movie => {
+              const { id, korean_title, country, release_date, thumbnail_img } =
+                movie;
+              return (
+                <MainSectionMovieList
+                  width={width}
+                  key={id}
+                  title={korean_title}
+                  country={country}
+                  releaseDate={release_date}
+                  thumbnailImgUrl={thumbnail_img}
+                />
+              );
+            })}
+          </ul>
+          <div></div>
         </section>
-      );
-    }
+        <button onClick={this.handleNextSliding} className="mainMovieNextBtn">
+          <i class="fas fa-chevron-right"></i>
+        </button>
+      </section>
+    );
   }
 }
+
+// }
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,11 +2,46 @@ import React from 'react';
 import MainSection from './Components/MainSection/MainSection';
 import './Main.scss';
 export default class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movieInformationList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://192.168.25.36:8000/movies/movies', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(movieInformationList => {
+        this.setState({
+          movieInformationList: movieInformationList,
+        });
+      });
+  }
+
   render() {
+    const { movieInformationList } = this.state;
+    const listTitle = Object.keys(movieInformationList);
+    const listTitleIndex = [];
+    for (let i = 0; i < listTitle.length; i++) {
+      listTitleIndex.push(i);
+    }
+    console.log(listTitleIndex);
     return (
-      <section className="main">
-        <MainSection />
-      </section>
+      <>
+        {movieInformationList && (
+          <section className="main">
+            {listTitleIndex.map(el => (
+              <MainSection
+                listTitle={listTitle[el]}
+                movieInformationList={movieInformationList[listTitle[el]]}
+              />
+            ))}
+          </section>
+        )}
+      </>
     );
   }
 }
