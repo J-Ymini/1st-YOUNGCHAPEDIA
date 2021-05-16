88d3651f12ad67b5035639952b7ca230b1f43ccf
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-16 15:10:26 +0900

    Modify : 영화 리스트 컴포넌트 분리 누락 파일

---
+++ src/pages/ReviewPage/component/ReviewMovieList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import StarRating from '../CommonComponents/StarRating';
+import './ReviewMovieList.scss';
+
+export default class ReviewMovieList extends Component {
+  render() {
+    const {
+      id,
+      movieTitle,
+      imgSrc,
+      movieReleaseDate,
+      movieCountry,
+      updateRatingCount,
+    } = this.props;
+    return (
+      <li className="reviewMovieList">
+        <img alt={`${movieTitle}포스터`} src={imgSrc} />
+        <div className="movieInfos">
+          <div className="movieInfoColumn">
+            <div className="reviewMovieTitle">
+              <span>{movieTitle}</span>
+              <span className="reviewIcon">
+                <FontAwesomeIcon icon={faEllipsisV} />
+              </span>
+            </div>
+            <div className="movieYearCountry">
+              {movieReleaseDate.slice(0, 4)}·{movieCountry}
+            </div>
+          </div>
+          <div className="movieInfoColumn">
+            <StarRating
+              id={id}
+              postRatings={postRatings}
+              updateRatingCount={updateRatingCount}
+            />
+          </div>
+        </div>
+      </li>
+    );
+  }
+}
