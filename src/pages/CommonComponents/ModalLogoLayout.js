4d12a30446c3e4065384e231202f50fa5b92906a
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-16 16:58:40 +0900

    Modify : 레이아웃 수정

--- src/pages/CommonComponents/ModalLogoLayout.js
+++ src/pages/CommonComponents/ModalLogoLayout.js
@@ -4,15 +4,15 @@ import './ModalLogoLayout.scss';
 export default class ModalLogoLayout extends Component {
   render() {
     return (
-      <>
+      <div className="modalSize">
         <header className="modalLogo">
           <h1>
-            <span>YOUGNCHA</span>
+            <span>YOUNGCHA</span>
             <span>PEDIA</span>
           </h1>
         </header>
         {this.props.children}
-      </>
+      </div>
     );
   }
 }
