47e9a9bd401184aadc5d3b027215c69a226ff8a7
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-16 21:53:09 +0900

    Modify : Router순서 수정

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
+import Footer from './pages/CommonComponents/Footer';
 // import login
 // import mypage
 import Navbar from './pages/CommonComponents/Navbar';
-import Footer from './pages/CommonComponents/Footer';
 
 class Routes extends React.Component {
   render() {
