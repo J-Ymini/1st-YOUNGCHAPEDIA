eb2b0ac625525d2f1ec2ab621e1e060ff96b2bea
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-16 23:25:24 +0900

    Add : 마이페이지 레이아웃 완성

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 import './MyPage.scss';
 
 export default class MyPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <section className="myPageSection">
         <div className="myPageContainer">
-          <div className="myHeader">
-            <i class="fas fa-cog"></i>
-          </div>
+          <header className="myHeader">
+            <div className="myHeaderShadow" />
+            <button>
+              <i class="fas fa-cog" />
+            </button>
+          </header>
           <div className="myContents">
             <header className="myProfile">
               <div className="myProfileColumn">
                 <img
                   alt="userProfile"
+                  className="myPageProfileImg"
                   src="https://an2-img.amz.wtchn.net/image/v2/1bcea716e2558d00efb37cc2422675b4.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKaVlXTnJaM0p2ZFc1a0lqcDdJbklpT2pJMU5Td2laeUk2TWpVMUxDSmlJam95TlRWOUxDSmpjbTl3SWpwMGNuVmxMQ0pvWldsbmFIUWlPalF3TUN3aWNHRjBhQ0k2SWk5Mk1TOWhjR1pvZVhaNmJURXpNVGh4ZFRCcGFHVnFZeUlzSW5GMVlXeHBkSGtpT2pnd0xDSjNhV1IwYUNJNk1qZ3dmUS5ONTVQd1ZrZm5iZDR2d1o3RUpZSTRIWlg2VUxlMS1pLWRkdk1MT3hMd2tZ"
                 />
               </div>
@@ -26,24 +34,24 @@ export default class MyPage extends Component {
             </header>
             <div className="analyzeTest">
               <div>
-                <button>취향분석</button>
+                <button className="analyzeBtn">
+                  <i class="fas fa-chart-bar" />
+                  취향분석
+                </button>
               </div>
             </div>
             <div className="contentBanners">
-              <div className="myMovies">
-                <button>
-                  <i class="fas fa-film"></i>
-                  <p className="contentBannersColumn">영화</p>
-                  <p className="contentBannersColumn">
-                    <span>★</span>
-                    <span>14</span>
-                  </p>
-                  <p className="contentBannersColumn">
-                    <span>보고싶어요</span>
-                    <span>1</span>
-                  </p>
-                </button>
-              </div>
+              <button className="myMovies">
+                <p className="contentBannersColumn">영화</p>
+                <p className="contentBannersColumn">
+                  <span>★</span>
+                  <span>14</span>
+                </p>
+                <p className="contentBannersColumn">
+                  <span>보고싶어요</span>
+                  <span>1</span>
+                </p>
+              </button>
             </div>
           </div>
         </div>
