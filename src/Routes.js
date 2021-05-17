532f4f9c5b5e0daabe7eae961a4bf4a3b73bfbfc
Daseul <dls2tmfs2@gmail.com>
2021-05-17 11:33:31 +0900

    Fixed : conflict 수정

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,8 @@ import Footer from './pages/CommonComponents/Footer';
 import Navbar from './pages/CommonComponents/Navbar';
 import ReviewPage from './pages/ReviewPage/ReviewPage';
 import MovieDetail from './pages/MovieDetail/MovieDetail';
+import MyPage from './pages/MyPage/MyPage';
+
 class Routes extends React.Component {
   render() {
     return (
@@ -14,8 +16,7 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/moviedetail" component={MovieDetail}></Route>
           <Route exact path="/review" component={ReviewPage}></Route>
-          {/* <Route exact path="/contents" component={}></Route> */}
-          {/* <Route exact path="/myPage" component={}></Route> */}
+          <Route exact path="/mypage" component={MyPage}></Route>;
         </Switch>
         {location.pathname !== '/review' && <Footer />}
       </Router>
@@ -23,7 +24,9 @@ class Routes extends React.Component {
   }
 }
 export default Routes;
+
 /* global history */
 /* global location */
 /* global window */
+
 /* eslint no-restricted-globals: ["off"] */
