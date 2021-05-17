0f3fad3e34afa86cee20cb883c39a7a7558493bf
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-17 10:43:59 +0900

    Fix : 컨플릭트 대비 route 통일

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
 import Footer from './pages/CommonComponents/Footer';
-// import login
-// import mypage
 import Navbar from './pages/CommonComponents/Navbar';
-import MyPage from './pages/MyPage/MyPage';
+import ReviewPage from './pages/ReviewPage/ReviewPage';
 import MovieDetail from './pages/MovieDetail/MovieDetail';
+import MyPage from './pages/MyPage/MyPage';
 
 class Routes extends React.Component {
   render() {
@@ -16,14 +15,19 @@ class Routes extends React.Component {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Main}></Route>
-          {/* <Route exact path="/movieDetail" component={MovieDetail}></Route> */}
-          {/* <Route exact path="/contents" component={}></Route> */}
-          <Route exact path="/mypage" component={MyPage}></Route>
+          <Route exact path="/moviedetail" component={MovieDetail}></Route>
+          <Route exact path="/review" component={ReviewPage}></Route>
+          <Route exact path="/mypage" component={MyPage}></Route>;
         </Switch>
-        <Footer />
+        {location.pathname !== '/review' && <Footer />}
       </Router>
     );
   }
 }
-
 export default Routes;
+
+/* global history */
+/* global location */
+/* global window */
+
+/* eslint no-restricted-globals: ["off"] */
