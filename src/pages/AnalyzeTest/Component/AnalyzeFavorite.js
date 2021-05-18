f62360dae5ab85dd5d021626f48a854c5eb69f18
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-18 16:50:01 +0900

    Add : key관련 todo 주석 추가

--- src/pages/AnalyzeTest/Component/AnalyzeFavorite.js
+++ src/pages/AnalyzeTest/Component/AnalyzeFavorite.js
@@ -8,6 +8,7 @@ export default class AnalyzeFavorite extends Component {
       <>
         <ul className="topResults">
           {favDatas.slice(0, 3).map((favData, index) => (
+            // Todo : id값이 같이 오는지 확인되지 않아서 확인되면 id값으로 바꾸거나 다른 방법으로 key값 대체하겠습니당
             <li className="resultRow" key={index}>
               {favData.genre || favData.country}
               <span>
@@ -19,6 +20,7 @@ export default class AnalyzeFavorite extends Component {
         </ul>
         <ul className="resultLists">
           {favDatas.slice(3).map((favData, index) => (
+            // Todo : id값이 같이 오는지 확인되지 않아서 확인되면 id값으로 바꾸거나 다른 방법으로 key값 대체하겠습니당
             <li className="resultList" key={index}>
               {favData.genre || favData.country}
               <span>
