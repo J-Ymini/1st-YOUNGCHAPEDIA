23eafe561fc96200e2217219b8d8636680ab4a1a
Daseul <dls2tmfs2@gmail.com>
2021-05-18 16:46:29 +0900

    중간 백업

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,89 @@
 import React from 'react';
 import MainSection from './Components/MainSection/MainSection';
 import './Main.scss';
-
 export default class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movieInformationList: [],
+    };
+  }
+
+  getBoxOfficeData = () => {
+    fetch('data/boxoffice.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(movieList => {
+        this.setState({
+          movieInformationList: [
+            ...this.state.movieInformationList,
+            movieList.MESSAGE[0],
+          ],
+        });
+      });
+  };
+
+  getNetflixData = () => {
+    fetch('data/netflix.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(movieList => {
+        this.setState({
+          movieInformationList: [
+            ...this.state.movieInformationList,
+            movieList.MESSAGE[0],
+          ],
+        });
+      });
+  };
+
+  getWatchaData = () => {
+    fetch('data/watcha.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(movieList => {
+        this.setState({
+          movieInformationList: [
+            ...this.state.movieInformationList,
+            movieList.MESSAGE[0],
+          ],
+        });
+      });
+  };
+
+  getMockdata = () => {
+    fetch('data/movieMockData.json', {
+      method: 'GET',
+    }).then(res => res.json()).then;
+  };
+
+  componentDidMount() {
+    this.getBoxOfficeData();
+    this.getNetflixData();
+    this.getWatchaData();
+  }
+
   render() {
     return (
-      <section className="main">
-        <MainSection />
-      </section>
+      <>
+        {this.state.movieInformationList && (
+          <section className="main">
+            {this.state.movieInformationList.map(
+              (listElement, movieTitleindex) => {
+                return (
+                  <MainSection
+                    movieInformationList={listElement}
+                    movieTitleIndex={movieTitleindex}
+                  />
+                );
+              }
+            )}
+          </section>
+        )}
+      </>
     );
   }
 }
