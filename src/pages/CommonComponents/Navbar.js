81702ef92a8f11b298e2f3ad2f522ff9f68d9f9d
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-19 20:14:29 +0900

    Add : NavBar 마이페이지 버튼 연결, 회원가입-로그인 전환 추가

--- src/pages/CommonComponents/Navbar.js
+++ src/pages/CommonComponents/Navbar.js
@@ -47,10 +47,23 @@ class Navbar extends Component {
 
   clickLogout = () => {
     localStorage.removeItem('TOKEN');
+    localStorage.removeItem('NAME');
     this.setState({
       isUserLogined: false,
     });
     this.props.history.push('/');
+  };
+
+  goToLoginModal = () => {
+    this.setState({
+      isSignBtnClicked: false,
+    });
+  };
+
+  goToSignInModal = () => {
+    this.setState({
+      isSignBtnClicked: true,
+    });
   };
 
   render() {
@@ -61,6 +74,8 @@ class Navbar extends Component {
       clickLogin,
       clickLogout,
       checkUserLogined,
+      goToLoginModal,
+      goToSignInModal,
     } = this;
     const logoutedBtn = (
       <>
@@ -95,8 +110,11 @@ class Navbar extends Component {
             closeModal={closeModal}
             childComponent={
               <LoginSignInForm
+                closeModal={closeModal}
                 checkUserLogined={checkUserLogined}
                 isSignBtnClicked={isSignBtnClicked}
+                goToLoginModal={goToLoginModal}
+                goToSignInModal={goToSignInModal}
               />
             }
           />
