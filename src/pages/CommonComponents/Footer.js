159ff65075c0a369fdcba6bd9eb327b5f98f0e00
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-19 19:36:40 +0900

    Add : footer 리뷰 반영 및 scss 폰트 rem단위로 수정

--- src/pages/CommonComponents/Footer.js
+++ src/pages/CommonComponents/Footer.js
@@ -1,52 +1,81 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import Modal from './Modal';
+import EasterEgg from './EasterEgg';
 import './Footer.scss';
 
 class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modalOpened: false,
+    };
+  }
+
+  closeModal = () => {
+    this.setState({
+      modalOpened: false,
+    });
+  };
+
+  handleEasterEgg = () => {
+    this.setState({
+      modalOpened: true,
+    });
+  };
+
   render() {
     const currentPage = this.props.location.pathname;
+    if (currentPage === '/review' || currentPage === '/mytest') return null;
     return (
       <>
-        {currentPage !== '/review' && (
-          <footer className="commonFooter">
-            <section className="evaluationFooter">
-              <span>
-                지금까지 <span className="footerCounter">얼마</span>의 평가
+        {this.state.modalOpened && (
+          <Modal closeModal={this.closeModal} childComponent={<EasterEgg />} />
+        )}
+        <footer className="commonFooter">
+          <section className="evaluationFooter">
+            <span>
+              지금까지{' '}
+              <span onClick={this.handleEasterEgg} className="footerCounter">
+                얼마
               </span>
-            </section>
-            <section className="descriptionFooter">
-              <div className="siteDescription">
-                <ul>
-                  <li>
-                    <span>이 사이트는 공식 왓챠 사이트가 아닙니다</span>
-                  </li>
-                  <li>
-                    <span>이 사이트는 공식 왓챠 사이트와 관련이 없습니다</span>
-                  </li>
-                  <li>
-                    <span>
-                      이 사이트는 학생들이 공부용으로 만든 클론 프로젝트
-                      사이트입니다
-                    </span>
-                  </li>
-                </ul>
-              </div>
-              <div className="memberDescription">
-                <ul>
-                  {MEMBER_LIST.map(member => {
+              의 평가
+            </span>
+          </section>
+          <section className="descriptionFooter">
+            <div className="siteDescription">
+              <ul>
+                <li>
+                  <span>이 사이트는 공식 왓챠 사이트가 아닙니다</span>
+                </li>
+                <li>
+                  <span>이 사이트는 공식 왓챠 사이트와 관련이 없습니다</span>
+                </li>
+                <li>
+                  <span>
+                    이 사이트는 학생들이 공부용으로 만든 클론 프로젝트
+                    사이트입니다
+                  </span>
+                </li>
+              </ul>
+            </div>
+            <div className="memberDescription">
+              <ul>
+                {MEMBER_LIST.map(member => {
+                  return (
                     <li key={member.id}>
                       <button>
                         <a href={member.blog} target="_blank">
                           <span>{member.name}</span>
                         </a>
                       </button>
-                    </li>;
-                  })}
-                </ul>
-              </div>
-            </section>
-          </footer>
-        )}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </section>
+        </footer>
       </>
     );
   }
