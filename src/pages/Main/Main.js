fef45aaca429ef77ce4215930a7a9979d12bffdf
Daseul <dls2tmfs2@gmail.com>
2021-05-19 15:59:11 +0900

    중간백업

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -6,11 +6,14 @@ export default class Main extends React.Component {
     super();
     this.state = {
       movieInformationList: [],
+      preItems: 0,
+      items: 1,
     };
   }
 
-  getBoxOfficeData = () => {
-    fetch('data/boxoffice.json', {
+  //백엔드에서 데이터 받아오는 함수
+  getData = address => {
+    return fetch(address, {
       method: 'GET',
     })
       .then(res => res.json())
@@ -24,46 +27,61 @@ export default class Main extends React.Component {
       });
   };
 
-  getNetflixData = () => {
-    fetch('data/netflix.json', {
+  getMockData = () => {
+    return fetch('data/movieMockData.json', {
       method: 'GET',
     })
       .then(res => res.json())
       .then(movieList => {
+        let dataToAdd = movieList.slice(this.state.preItems, this.state.items);
         this.setState({
           movieInformationList: [
             ...this.state.movieInformationList,
-            movieList.MESSAGE[0],
+            ...dataToAdd,
           ],
         });
+        console.log(dataToAdd);
+        console.log(this.state.items);
       });
   };
 
-  getWatchaData = () => {
-    fetch('data/watcha.json', {
-      method: 'GET',
-    })
-      .then(res => res.json())
-      .then(movieList => {
-        this.setState({
-          movieInformationList: [
-            ...this.state.movieInformationList,
-            movieList.MESSAGE[0],
-          ],
-        });
+  infiniteScroll = () => {
+    if (
+      document.documentElement.scrollTop >=
+      document.documentElement.scrollHeight -
+        document.documentElement.clientHeight
+    ) {
+      this.setState({
+        preItems: this.state.preItems + 1,
+        items: this.state.items,
       });
+      fetch('data/movieMockData.json', {
+        method: 'GET',
+      })
+        .then(res => res.json())
+        .then(res => {
+          let result = res.slice(this.state.preitem, this.state.item);
+          this.setState({
+            movieInformationList: [
+              ...this.state.movieInformationList,
+              ...result,
+            ],
+          });
+        });
+    }
   };
 
-  getMockdata = () => {
-    fetch('data/movieMockData.json', {
-      method: 'GET',
-    }).then(res => res.json()).then;
-  };
-
+  // removeInfiniteScroll = () => {
+  //   if(this.state.)
+  // }
+  // 함수 실행
   componentDidMount() {
-    this.getBoxOfficeData();
-    this.getNetflixData();
-    this.getWatchaData();
+    this.getData('data/boxoffice.json')
+      .then(this.getData('data/netflix.json'))
+      .then(this.getData('data/watcha.json'))
+      .then(this.getMockData());
+    window.addEventListener('scroll', this.infiniteScroll);
+    console.log(this.state.item);
   }
 
   render() {
@@ -71,16 +89,14 @@ export default class Main extends React.Component {
       <>
         {this.state.movieInformationList && (
           <section className="main">
-            {this.state.movieInformationList.map(
-              (listElement, movieTitleindex) => {
-                return (
-                  <MainSection
-                    movieInformationList={listElement}
-                    movieTitleIndex={movieTitleindex}
-                  />
-                );
-              }
-            )}
+            {this.state.movieInformationList.map((listElement, movieTitle) => {
+              return (
+                <MainSection
+                  movieInformationList={listElement}
+                  movieTitle={movieTitle}
+                />
+              );
+            })}
           </section>
         )}
       </>
