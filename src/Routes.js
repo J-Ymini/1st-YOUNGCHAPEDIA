121c0669ed3954b209a59f7981fb2df9c206c611
Daseul <dls2tmfs2@gmail.com>
2021-05-19 12:12:05 +0900

    Routes 수정

--- src/Routes.js
+++ src/Routes.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Footer from './pages/CommonComponents/Footer';
 import Navbar from './pages/CommonComponents/Navbar';
-// import ReviewPage from './pages/ReviewPage/ReviewPage';
-// import MovieDetail from './pages/MovieDetail/MovieDetail';
-// import MyPage from './pages/MyPage/MyPage';
+import ReviewPage from './pages/ReviewPage/ReviewPage';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import MyPage from './pages/MyPage/MyPage';
 
 class Routes extends React.Component {
   render() {
@@ -15,9 +15,9 @@ class Routes extends React.Component {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Main}></Route>
-          {/* <Route exact path="/moviedetail" component={MovieDetail}></Route>
+          <Route exact path="/moviedetail" component={MovieDetail}></Route>
           <Route exact path="/review" component={ReviewPage}></Route>
-          <Route exact path="/mypage" component={MyPage}></Route> */}
+          <Route exact path="/mypage" component={MyPage}></Route>
         </Switch>
         <Footer />
       </Router>
