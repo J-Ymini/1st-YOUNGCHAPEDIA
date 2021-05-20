b96fe895256ed0e607fb4f8c2560355483c2383b
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-20 10:12:52 +0900

    Modify : 불필요한 파일 삭제

--- src/pages/CommonComponents/EasterEgg.js
+++ src/pages/CommonComponents/EasterEgg.js
@@ -3,12 +3,6 @@ import './EasterEgg.scss';
 
 export default class EasterEgg extends Component {
   render() {
-    return (
-      <div className="easterEgg">
-        <header className="easterEggHeader">Special Thanks To</header>
-        <p className="thanksPeople">⭐갓연욱⭐</p>
-        <p className="thanksPeople">⭐갓연우⭐</p>
-      </div>
-    );
+    return <div className="easterEgg"></div>;
   }
 }
